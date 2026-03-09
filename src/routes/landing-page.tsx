9f8e3924d26bf94818dd9678345809365f55669f
Ntale Samad <swamaduntale98@gmail.com>
2026-03-10 01:39:45 +0300

    ✨ create landing page

---
+++ src/routes/landing-page.tsx
@@ -0,0 +1,29 @@
+import { CTABanner, Footer } from '@/components/landing-page/cta-and-footer'
+import { FeaturesSection } from '@/components/landing-page/FeaturesSection'
+import HeroSection from '@/components/landing-page/Hero-section'
+import HowItWorksSection from '@/components/landing-page/how-it-works-section'
+import NavBar from '@/components/landing-page/nav-bar'
+import { StatsSection } from '@/components/landing-page/stats-section'
+import { TestimonialsSection } from '@/components/landing-page/TestimonialsSection'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/landing-page')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+  <div>
+    <HeroSection/>
+    <StatsSection/>
+    <HowItWorksSection/>
+    <FeaturesSection />
+    <HowItWorksSection  />
+    <TestimonialsSection />
+    <CTABanner  />
+    <Footer/>
+    <NavBar/>
+  </div>
+  
+  )
+}
