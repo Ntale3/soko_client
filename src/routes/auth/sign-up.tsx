06b53075933f00c1700cbfda949e9c40043763f1
Ntale Samad <swamaduntale98@gmail.com>
2026-03-14 17:45:32 +0300

    ✨ create auth pages routes

---
+++ src/routes/auth/sign-up.tsx
@@ -0,0 +1,25 @@
+import AuthLayout from '@/components/auth/auth-layout'
+import { RadioGroupChoiceCard } from '@/components/auth/sign-up-steps/choice-card'
+import { panel } from '@/components/auth/sign-up-panel'
+import { createFileRoute } from '@tanstack/react-router'
+import SignUpForm from '@/components/auth/sign-up-steps/sign-up-form'
+import { OnboardingChecklist } from '@/components/auth/OnboardingChecklist'
+import { CheckList } from '@/components/auth/sign-up-steps/check-list'
+
+export const Route = createFileRoute('/auth/sign-up')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <AuthLayout panel={panel}>
+      <div >
+       
+          {/* <RadioGroupChoiceCard/> */}
+          {/* <SignUpForm/> */}
+          {/* <OnboardingChecklist/> */}
+          <CheckList/>
+      </div>
+    </AuthLayout>
+  )
+}
