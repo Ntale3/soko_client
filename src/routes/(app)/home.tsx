25412c600b0c33b6735f2f39fbdab680d65f0c77
Ntale Samad <swamaduntale98@gmail.com>
2026-03-21 16:37:19 +0300

    ♻️  finish up home page UI

--- src/routes/(app)/home.tsx
+++ src/routes/(app)/home.tsx
@@ -1,3 +1,11 @@
+import AiBanner from '@/components/Home-page/ai-banner'
+import AIMatchedFarmersSection from '@/components/Home-page/ai-matched-farmers-section'
+import PricePrediction from '@/components/Home-page/ai-price-predictions'
+import Categories from '@/components/Home-page/categories'
+import { FreshListingsSection } from '@/components/Home-page/fresh-listing-section'
+import { LatestArticlesSection } from '@/components/Home-page/latest-articles-section'
+import StickyHeader from '@/components/Home-page/sticky-header'
+import { blogs, products } from '@/constants/dummy-data'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(app)/home')({
@@ -5,5 +13,34 @@ export const Route = createFileRoute('/(app)/home')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/(app)/home"!</div>
+
+  return (
+    <div className='pb-24 min-h-screen bg-background '>
+      {/* Sticky Header */}
+      <StickyHeader/>
+
+      <div className='flex flex-col gap-7 px-4 pt-5 no-scrollbar '>
+        {/* AI Banner */}
+        <AiBanner/>
+
+         {/* Categories */}
+         <Categories/>
+
+         {/* ai - price - predictions */}
+         <PricePrediction/>
+
+          {/* ai - farmer- recommendations */}
+
+          <AIMatchedFarmersSection/>
+
+          {/* Fresh listing section */}
+          <FreshListingsSection products={products}/>
+
+          {/* latest blog section */}
+          <LatestArticlesSection blogs={blogs}/>
+
+      </div>
+
+    </div>
+  )
 }
